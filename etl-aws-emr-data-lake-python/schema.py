73222151ea265e7294c9c7887ddb357feba05cf8
shravan kuchkula <shravan.kuchkula@gmail.com>
2019-09-14 15:50:02 +0000

    Data Lakes initial commit

---
+++ etl-aws-emr-data-lake-python/schema.py
@@ -0,0 +1,37 @@
+# song table
+song_id = "song_id"
+title = "title"
+artist_id = "artist_id"
+year = "year"
+duration = "duration"
+
+# artist table
+artist_id = "artist_id"
+name = "artist_name"
+location = "artist_location"
+latitude = "artist_latitude"
+longitude = "artist_longitude"
+
+# user table
+user_id = "userId"
+first_name = "firstName"
+last_name = "lastName"
+gender = "gender"
+level = "level"
+
+# time table
+start_time = "timestamp"
+hour = "hour"
+day = "day"
+week = "week"
+month = "month"
+year = "year"
+weekday = "weekday"
+
+# songplay table
+songplay_id = "songplay_id"
+session_id = "sessionId"
+locationSP = "location"
+user_agent = "userAgent"
+song = "song"
+artist = "artist"
